77c3c2af2cc5a045148c2ced1ebc1a7f4861db09
ShruthiSiva <ssiva@bu.edu>
2023-07-21 16:28:05 -0600

    expiration service boilerplate

---
+++ expiration/src/index.ts
@@ -0,0 +1,36 @@
+import { natsWrapper } from "./nats-wrapper";
+
+const start = async () => {
+  // Always check that we have the required env variables during startup.
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL must be defined");
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must be defined");
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID must be defined");
+  }
+
+  try {
+    // Cluster ID is "ticketing" as was specified in the nats-depl file.
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed");
+      process.exit();
+    });
+
+    // See explanation in /ticketing/nats-test/src/listener.ts
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+start();
